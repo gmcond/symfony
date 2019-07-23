78a5dc84a21eed4609a1e69fc44ef53d732daa58
gmasprakton <gmas@prakton.es>
2019-07-23 11:02:49 +0200

    Start Symfony Project.

---
+++ src/Controller/HolaController.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Controller;
+ 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+ 
+# El nombre de la clase debe coincidir con el nombre del archivo .php 
+class HolaController
+{
+    public function hola()
+    {
+        return new Response(
+            '<html><body>Hola mundo desde <strong>Symfony</strong><br>parzibyte.me</body></html>'
+        );
+    }
+    /**
+     * @Route("/anotaciones", name="probar_anotaciones")
+     */
+    public function anotaciones(){
+        return new Response(
+            '<html><body>Ahora usamos las anotaciones</body></html>'
+        );
+    }
+}
